b65fd585c7de340b0393f1a0844d5b55f3b6faa8
Guilhermino Lucas <guilherminolucasc@gmail.com>
2021-09-28 15:19:44 -0300

    refatorando

--- src/pages/FruitsPage/index.jsx
+++ src/pages/FruitsPage/index.jsx
@@ -1,14 +1,21 @@
 //importando useSelector do react-redux
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import { PageTitle, FruitName } from "./style.js";
+import { addFruit } from "../../store/modules/fruits/actions";
+import { useState } from "react";
 
 const FruitsPage = () => {
+  const [fruit, setFruit] = useState("");
+  const dispatch = useDispatch();
   //acessando o estado global e pegando o state fruits
   const fruits = useSelector((state) => state.fruits);
+  const handleAddFruit = () => dispatch(addFruit(fruit));
 
   return (
     <div>
-      <PageTitle>Top 3 frutas foda</PageTitle>
+      <PageTitle>Top 3 frutas</PageTitle>
+      <input onChange={(e) => setFruit(e.target.value)} />
+      <button onClick={handleAddFruit}>Adicionar fruta</button>
       {fruits.map((fruit) => (
         <FruitName key={fruit}> {fruit} </FruitName>
       ))}
